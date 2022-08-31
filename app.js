c4b0d1a5a6d18b5925a748263e2315ee6b27b55b
Nishchhal <nishchhal901@gmail.com>
2022-08-31 17:24:26 +0530

    ejs linked with express

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+
 
 app.get("/",function (req,res) {
-    res.send("Hii there !!")
+    var today = new Date();
+var currentDay = today.getDay();
+if (currentDay==6||currentDay==0) {
+    res.sendFile(__dirname+"/weekend.html");
+    
+}
+else{
+    res.sendFile(__dirname+"/weekday.html");
+    
+}
 })
 
 
